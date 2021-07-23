4db2775395b473ab8550fcfdca4a1b8cf2e67b14
Fabio A. Machado <macfabio@gmail.com>
2021-07-23 11:16:14 -0400

    prelim

--- R/PCMBMkappa.R
+++ R/PCMBMkappa.R
@@ -3,5 +3,5 @@
 #' @name PCMkappa
 #' @description Add on for PCMBase stablishing the kappa class of evolutionary model to investigate the proportionality between matrices in a phylogenetic context.
 #' @docType package
-#' @imports PCMBase
+#' @import PCMBase
 NULL
