15f1aa6d1f866d6fb410e2dd7bfd349ed65a29b1
Fabio A. Machado <macfabio@gmail.com>
2022-08-19 15:58:41 -0400

    erase PCMBMkappa

--- R/PCMBMkappa.R
+++
@@ -1,7 +0,0 @@
-#' PCMBMKappa
-#'
-#' @name PCMkappa
-#' @description Add on for PCMBase establishing the kappa class of evolutionary model to investigate the proportionality between matrices in a phylogenetic context.
-#' @docType package
-#' @import PCMBase
-NULL
