7c265c23eaa00519a76cd0758e34a22653da80d7
Fabio A. Machado <macfabio@gmail.com>
2022-08-19 17:18:14 -0400

    Update GeneratingDefaultModels.R

--- GeneratingDefaultModels.R
+++ GeneratingDefaultModels.R
@@ -1,5 +1,5 @@
 roxygen2::roxygenize()
 PCMBase::PCMGenerateModelTypes(baseTypes = list(BMkappa = "all"), sourceFile = "R/BMkappaDefaultTypes.R")
-PCMBase::PCMGenerateModelTypes(baseTypes = list(OUkappa = "all"), sourceFile = "R/OUkappaDefaultTypes.R")
-PCMBase::PCMGenerateModelTypes(baseTypes = list(BM_drift = "all"), sourceFile = "R/BM_driftDefaultTypes.R")
+PCMBase::PCMGenerateModelTypes(baseTypes = list(OUkappa = "default"), sourceFile = "R/OUkappaDefaultTypes.R")
+PCMBase::PCMGenerateModelTypes(baseTypes = list(BM_drift = "default"), sourceFile = "R/BM_driftDefaultTypes.R")
 roxygen2::roxygenize()
