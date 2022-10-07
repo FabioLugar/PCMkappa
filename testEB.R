be6ad6392df0c51cd176aba1a60b78df306ed5ed
Fabio A. Machado <macfabio@gmail.com>
2022-10-07 15:47:53 -0400

    fitting works

--- testEB.R
+++ testEB.R
@@ -6,25 +6,20 @@ library(ggtree)
 library(magrittr)
 library(dplyr)
 library(TreeSim)
-library(mvMORPH)
+library(foreach)
 
 set.seed(909)
-#star
+#running on a star phylogeny just to get EB trend-------
 tree<-stree(50)
 tree$edge.length<-rep(50,50)
-# tree<-add.random(tree=tree,n=500,edge.length = rep(0,500))
+tree<-add.random(tree=tree,n=500,edge.length = rep(0,500))
 plot(tree, show.tip.label=FALSE)
 tiplabels(pch=21)
 
-roxygen2::roxygenize()
 modelEB <- PCM(model = "EB", k = 1)
 modelEB$rho[]<- 1
 modelEB$Sigma_x[]<-1
 
-metaI<-PCMInfo(X = NULL,tree = tree,model = modelEB, nodeHeights=nodeHeights(tree))
-metaI$r
-
-sim<-PCMSim(tree,modelEB,X0 = 0,metaI = metaI)
 sim<-PCMSim(tree,modelEB,X0 = 0)
 
 sim <- sim[1,]
@@ -39,23 +34,16 @@ ggtree(tree_p, aes(color=trait),yscale = "trait",
        continuous = 'colour', size=1)+
   scale_color_viridis_c() + theme_minimal()
 
-simdat<-sim[tree$tip.label] %>% t
+#Simulationg and getting likelihoods------
 
-tree<-rtree(50)
+trees<-sim.bd.taxa(100,numbsim = 100,lambda = 1,mu = 0.5)
+liks<- foreach(i=seq_along(trees)) %do% {
+          tree<-trees[[i]]
+          sim<-PCMSim(tree,modelEB,X0 = 0)
+          PCMLik(t(sim[,tree$tip.label]), tree = tree, modelEB)
+        }
+summary(unlist(liks))
 
-PCMLik(simdat, tree = tree, modelEB)
+#ok. 
 
-# sim<-mvSIM(tree, nsim = 1, model = "EB",
-#       param = list(sigma=1, beta = -1))
-# sim <- sim[,1]
-# d <- data.frame(node = nodeid(tree, names(sim)),
-#                 trait = sim)
-# 
-# d$node[is.na(d$node)]<-rownames(d)[is.na(d$node)]
-# d$node <- as.numeric(d$node)
-# tree_p <- full_join(tree, d, by = 'node')
-# 
-# ggtree(tree_p, aes(color=trait),yscale = "trait", 
-#        continuous = 'colour', size=1)+
-#   scale_color_viridis_c() + theme_minimal()
-# 
+
